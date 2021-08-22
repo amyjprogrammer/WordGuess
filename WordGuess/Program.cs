156184a14a3be5c30218ce616f652a39b7d0790c
amyjprogrammer <amyjprogrammer@gmail.com>
2021-08-22 18:41:32 -0400

    Add project files.

---
+++ WordGuess/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WordGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+
+            Console.Write("Please enter your guess for the word: ");
+             string guess = Console.ReadLine();
+
+            while (guess != secretWord)
+            {
+                if (guess != secretWord)
+                {
+                    Console.WriteLine("Wrong word.");
+                    Console.Write("Please try again: ");
+                    guess = Console.ReadLine();
+                }
+                else
+                {
+                    break;
+                }
+            }  
+            
+            if (guess == secretWord)
+            {
+                Console.WriteLine("You won!");
+            }
+        }
+    }
+}
